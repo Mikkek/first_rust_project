1b151770d7d1032cdc519e51152313f87b5e5106
Mikkek <54848327+Mikkek@users.noreply.github.com>
2020-08-08 16:54:17 +0200

    final commit
    game now works

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+    let num = rand::thread_rng()
+        .gen_range(1, 101);
+
+    let mut res = false;
+
     println!("Guess the number!\nPlease input your guess");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+    while !res {
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("that is not a number!");
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&num) {
+            Ordering::Less => println!("too low!"),
+            Ordering::Equal => {
+                println!("correct! You win!");
+                res = true},
+            Ordering::Greater => println!("too high!")
+        }
+    }
 }
